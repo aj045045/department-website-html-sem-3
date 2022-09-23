8b49c44599abdbd3495c5e0ff3de095db9632d4f
aj <113114943+aj045045@users.noreply.github.com>
2022-09-23 11:18:01 +0530

    update

--- footer & header.js
+++ footer & header.js
@@ -16,7 +16,7 @@ let appHeader = `
                     </li>
                     <!--Add index.html-->
                     <li class="nav-item">
-                        <a class="nav-link text-light" href="courses.html"><b>ACADEMICS</b></a>
+                        <a class="nav-link text-light" href="academics.html"><b>ACADEMICS</b></a>
                     </li>
                     
                     <!--Add index.html-->
