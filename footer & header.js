1174ce25f1e3384e3f9500189753d26c6b4542c3
aj045045 <aj045045@gmail.com>
2022-11-16 22:59:33 +0530

    add sign in

--- footer & header.js
+++ footer & header.js
@@ -61,7 +61,7 @@ let appHeader = `
                                 </ul>
                                 </div>
                                 </nav>
-
+                                <button onclick="topFunction()" id="myBtn" title="Go to top">&uarr;</button>
 `;
 document.getElementById("header").innerHTML = appHeader;
 
@@ -131,3 +131,26 @@ var myLineChart = new Chart(ctxD, {
     responsive: true
   }
 });
+
+
+// scroll up button
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
